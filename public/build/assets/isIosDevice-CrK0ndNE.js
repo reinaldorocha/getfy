fa7ff4161513d0a437a15b6a13e364de34bb5c3b
Reinaldo Rocha <reinaldo.sousa@korp.com.br>
2026-09-18 19:42:08 -0300

    build: regenerate production assets

--- public/build/assets/isIosDevice-CrK0ndNE.js
+++
@@ -1,1 +0,0 @@
-function e(){if(typeof navigator>"u")return!1;const t=navigator.userAgent||"";return!!(/iPhone|iPod|iPad/i.test(t)||navigator.platform==="MacIntel"&&navigator.maxTouchPoints>1)}export{e as i};
